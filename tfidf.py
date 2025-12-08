04ee74d4a5fdb83bf52c4f036115103d166846ab
rka <fipu@Mac-mini-od-FIPU.local>
2025-12-08 12:15:12 +0100

    tfidf file

---
+++ tfidf.py
@@ -0,0 +1,11 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+corpus_file = open('.txt', 'r', encoding='utf-8')
+
+corpus = corpus_file.read()
+
+print("---")
+print(corpus)
+print(corpus)
+print("---")
+print(corpus_file)
